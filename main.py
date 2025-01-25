c445cf3a386f487d77bd4fc48dac6bd7fe1e135a
@oseev <fedoseev_sv@mail.ru>
2025-01-25 20:51:39 +0300

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import aiogram
+import asyncio
+import logging
+import sys
+from os import getenv
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+
+print('works')
