63c069ea65648b5a34755f1bab90c513fe55411f
oseev <fedoseev_sv@mail.ru>
2025-02-03 22:33:27 +0300

    2nd

--- main.py
+++ main.py
@@ -3,11 +3,35 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, html, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
+import logging
+from config import TOKEN
 
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-print('works')
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("Привет!")
+
+@dp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer('Это команда /help')
+
+@dp.message(F.text == 'Как дела?')
+async def how_are_u(message: Message):
+    await message.answer('OK')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except:
+        print('Exit')
